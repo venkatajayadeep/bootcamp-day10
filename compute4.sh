fdd4c93a46f2cbfc1bebc65482a451956adb67a2
venkatajayadeep <jayadeep101097@gmail.com>
2021-06-04 12:31:10 +0530

    intial

---
+++ compute4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Arithmetic Computation & Sorting"
+
+echo "Enter three numbers: "
+read a
+read b
+read c 
+
+result1=`echo | awk "{print $a+$b*$c}"`
+result2=`echo | awk "{print $a*$b+$c}"`
+result3=`echo | awk "{print $c+$a/$b}"`
+result4=`echo | awk "{print $a%$b+$c}"`
